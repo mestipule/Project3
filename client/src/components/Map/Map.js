8e936cbe23583b9219d37a1449993ff2a388b752
May Ewers <mestipul@gmail.com>
2019-06-01 11:51:23 -0400

    button working with the map

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -447,19 +447,5 @@ class Map extends React.Component {
     }
 }
 
-/** 
- * How to use
-class NewCompo extends Component {
-    render() {
-      return(
-          <Map
-       google={this.props.google}
-       center={{lat: 18.5204, lng: 73.8567}}
-       height='300px'
-       zoom={15}
-      />
-        )
-    }
-  }
-  */
+
 export default Map
